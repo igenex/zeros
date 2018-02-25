7023ce1311dc7b2821459bb4861eb9ade87b683d
Eugene <igenexxx@gmail.com>
2018-02-25 12:59:45 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let result = 0;
+  while(number > 0) {
+    number /= 5;
+    result += Math.floor(number);
+  }
+  result += number / 2;
+  return result;
 }
+
+/*
+getZerosCount(84583674); //21145911
+getZerosCount(15949280); //3987315
+getZerosCount(71619670); //17904912
+
+*/
